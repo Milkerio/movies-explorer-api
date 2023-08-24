0a30be2e50c701222f96aa7312f0dbba6efc92e1
Vladimir Metelev <metelev_vladimir@mail.ru>
2023-08-24 16:07:02 +0300

    some fixes

--- routes/movie.js
+++ routes/movie.js
@@ -21,7 +21,7 @@ router.post('/', celebrate({
 }), createMovie);
 router.delete('/:_id', celebrate({
   params: Joi.object().keys({
-    movieId: Joi.string().length(24).required(),
+    movieId: Joi.string().length(24).hex().required(),
   }),
 }), deleteMovie);
 
